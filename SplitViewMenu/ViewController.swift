d143573b8ee8ec1383a6f1414a3021e6192ff67c
Stephen Anthony <steve@darjeelingapps.com>
2021-01-14 14:10:53 +0000

    Initial Commit

---
+++ SplitViewMenu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SplitViewMenu
+//
+//  Created by Stephen Anthony on 14/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
