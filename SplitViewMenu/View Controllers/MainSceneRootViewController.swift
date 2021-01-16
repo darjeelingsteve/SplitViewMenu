24cd85431dee7b7acb2aa978e6fdd466dd83573d
Stephen Anthony <steve@darjeelingapps.com>
2021-01-16 14:44:30 +0000

    Add MainSceneRootViewController

---
+++ SplitViewMenu/View Controllers/MainSceneRootViewController.swift
@@ -0,0 +1,43 @@
+//
+//  MainSceneRootViewController.swift
+//  SplitViewMenu
+//
+//  Created by Stephen Anthony on 16/01/2021.
+//
+
+import UIKit
+
+/// The view controller responsible for managing the user interface presented in
+/// the application's main scene.
+final class MainSceneRootViewController: UIViewController {
+    private let childSplitViewController: UISplitViewController = {
+        let childSplitViewController = UISplitViewController(style: .doubleColumn)
+        childSplitViewController.preferredDisplayMode = .oneBesideSecondary
+        childSplitViewController.preferredSplitBehavior = .tile
+        return childSplitViewController
+    }()
+    
+    private let secondaryColumnNavigationController = UINavigationController(rootViewController: NoPresenterSelectedViewController())
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addChild(childSplitViewController)
+        view.addSubview(childSplitViewController.view)
+        childSplitViewController.didMove(toParent: self)
+        
+        let presentersMenuViewController = PresentersMenuViewController()
+        presentersMenuViewController.delegate = self
+        childSplitViewController.preferredDisplayMode = .oneBesideSecondary
+        childSplitViewController.preferredSplitBehavior = .tile
+        childSplitViewController.setViewController(presentersMenuViewController, for: .primary)
+        childSplitViewController.setViewController(secondaryColumnNavigationController, for: .secondary)
+        childSplitViewController.setViewController(UINavigationController(rootViewController: NoPresenterSelectedViewController()), for: .compact)
+    }
+}
+
+// MARK: - PresentersMenuViewControllerDelegate
+extension MainSceneRootViewController: PresentersMenuViewControllerDelegate {
+    func presentersMenuViewController(_ presentersMenuViewController: PresentersMenuViewController, didSelectPresenter presenter: Presenter) {
+        secondaryColumnNavigationController.setViewControllers([PresenterDetailsViewController(presenter: presenter)], animated: false)
+    }
+}
