1b103d0d4b3677aaa0b1b0efd7b1aa94cfa1cefd
Stephen Anthony <steve@darjeelingapps.com>
2021-01-16 17:14:05 +0000

    Support Right-to-Left language layout

--- SplitViewMenu/View Controllers/PrimaryNavigationLayoutFixingSplitViewController.swift
+++ SplitViewMenu/View Controllers/PrimaryNavigationLayoutFixingSplitViewController.swift
@@ -25,19 +25,30 @@ final class PrimaryNavigationLayoutFixingSplitViewController: UISplitViewControl
                 return
             }
             
-            let expectedLeftSafeAreaInset: CGFloat = -viewController.view.frame.origin.x
-            if viewController.view.safeAreaInsets.left != expectedLeftSafeAreaInset {
-                /// Constrain to a minimum additional left inset of 0 to prevent
-                /// incorrect layouts when the split view transitions from
-                /// compact to regular width.
-                viewController.additionalSafeAreaInsets.left = max(0, expectedLeftSafeAreaInset - viewController.view.safeAreaInsets.left)
-            }
+            self.applyAdditionalSafeAreaInsets(to: viewController)
             if let childNavigationController = viewController.children.first as? UINavigationController {
                 /// Mark the navigation bar as needing layout to ensure that a
                 /// fresh layout pass occurs, which makes sure that
                 /// `displayModeButtonItem` is visible.
                 childNavigationController.navigationBar.setNeedsLayout()
             }
+        }
+    }
+    
+    private func applyAdditionalSafeAreaInsets(to viewController: UIViewController) {
+        switch viewController.traitCollection.layoutDirection {
+        case .leftToRight, .unspecified:
+            let expectedLeftSafeAreaInset: CGFloat = -viewController.view.frame.origin.x
+            if viewController.view.safeAreaInsets.left != expectedLeftSafeAreaInset, expectedLeftSafeAreaInset > 0 {
+                viewController.additionalSafeAreaInsets.left = expectedLeftSafeAreaInset - viewController.view.safeAreaInsets.left
+            }
+        case .rightToLeft:
+            let expectedRightSafeAreaInset: CGFloat = view.convert(viewController.view.bounds, from: viewController.view).maxX - view.bounds.maxX
+            if viewController.view.safeAreaInsets.right != expectedRightSafeAreaInset, expectedRightSafeAreaInset > 0 {
+                viewController.additionalSafeAreaInsets.right = expectedRightSafeAreaInset - viewController.view.safeAreaInsets.right
+            }
+        @unknown default:
+            fatalError("Unsupported layout direction")
         }
     }
 }
